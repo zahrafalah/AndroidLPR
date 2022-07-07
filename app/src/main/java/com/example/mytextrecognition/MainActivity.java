f63e7f209ac3ba7cd65c1054f99ec7b5f88a6fc6
Sepideh <sepidehfallah2@gmail.com>
2022-07-07 12:08:50 -0700

    gray scale and flip frames coming from the opencv camera

--- app/src/main/java/com/example/mytextrecognition/MainActivity.java
+++ app/src/main/java/com/example/mytextrecognition/MainActivity.java
@@ -12,102 +12,208 @@ import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.imgproc.Imgproc;
+
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
-    SurfaceView cameraView;
-    TextView textView;
-    CameraSource cameraSource;
-    final int RequestCameraPermissionId = 1001;
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+//    SurfaceView cameraView;
+//    TextView textView;
+//    CameraSource cameraSource;
+//    final int RequestCameraPermissionId = 1001;
+
+    CameraBridgeViewBase cameraBridgeViewBase;
+    BaseLoaderCallback baseLoaderCallback;
+    int counter = 0;
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        switch (requestCode) {
+//            case RequestCameraPermissionId:
+//                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+//                        return;
+//                    }
+//                    try {
+//                        cameraSource.start(cameraView.getHolder());
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//
+//                    }
+//                }
+//        }
+//    }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.CameraView);
+//        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+//        cameraBridgeViewBase.setCvCameraViewListener(this);
+//
+//        cameraView = (SurfaceView) findViewById(R.id.surface_view);
+//        textView = (TextView) findViewById(R.id.text_view);
+//        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+//        if (!textRecognizer.isOperational()) {
+//            Log.w("MainActivity", "Detector Dependencies are not yet available");
+//        }else {
+//            cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer).setFacing(CameraSource.CAMERA_FACING_BACK).setRequestedPreviewSize(1280, 1024).setRequestedFps(2.0f).setAutoFocusEnabled(true).build();
+//            cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+//                @Override
+//                public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+//                    try {
+//                        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+//                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA},RequestCameraPermissionId);
+//                            return;
+//                        }
+//                        cameraSource.start(cameraView.getHolder());
+//                    }catch(IOException e){
+//                        e.printStackTrace();
+//                    }
+//                }
+//
+//                @Override
+//                public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+//
+//                }
+//
+//                @Override
+//                public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+//                    cameraSource.stop();
+//                }
+//            });
+//            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+//                @Override
+//                public void release() {
+//
+//                }
+//
+//                @Override
+//                public void receiveDetections(Detector.Detections<TextBlock> detections) {
+//                    final SparseArray<TextBlock> items = detections.getDetectedItems();
+//                    if(items.size() != 0){
+//                        textView.post(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                StringBuilder stringBuilder = new StringBuilder();
+//                                for(int i = 0; i< items.size(); i++){
+//                                    TextBlock item = items.valueAt(i);
+//                                    stringBuilder.append(item.getValue());
+//                                    stringBuilder.append("/n");
+//                                }
+//                                textView.setText(stringBuilder.toString());
+//                            }
+//                        });
+//                    }
+//                }
+//            });
+//
+//            }
+//
+//    }
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+
+    cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.CameraView);
+    cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+    cameraBridgeViewBase.setCvCameraViewListener(this);
+
+
+    //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    baseLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            super.onManagerConnected(status);
+
+            switch(status){
+
+                case BaseLoaderCallback.SUCCESS:
+                    cameraBridgeViewBase.enableView();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+
+
+        }
+
+    };
+
+
+
+
+}
+
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case RequestCameraPermissionId:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                        return;
-                    }
-                    try {
-                        cameraSource.start(cameraView.getHolder());
-                    } catch (IOException e) {
-                        e.printStackTrace();
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Mat frame = inputFrame.rgba();
+        if (counter % 2 == 0){
+            Core.flip(frame, frame, 1);
+            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2GRAY);
+        }
+        counter = counter + 1;
+        return frame;
+    }
 
-                    }
-                }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()){
+            Toast.makeText(getApplicationContext(),"There's a problem, yo!", Toast.LENGTH_SHORT).show();
+        }
+
+        else
+        {
+            baseLoaderCallback.onManagerConnected(baseLoaderCallback.SUCCESS);
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(cameraBridgeViewBase!=null){
+            cameraBridgeViewBase.disableView();
         }
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cameraBridgeViewBase!=null){
+            cameraBridgeViewBase.disableView();
+        }
+    }
 
-        cameraView = (SurfaceView) findViewById(R.id.surface_view);
-        textView = (TextView) findViewById(R.id.text_view);
-        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
-        if (!textRecognizer.isOperational()) {
-            Log.w("MainActivity", "Detector Dependencies are not yet available");
-        }else {
-            cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer).setFacing(CameraSource.CAMERA_FACING_BACK).setRequestedPreviewSize(1280, 1024).setRequestedFps(2.0f).setAutoFocusEnabled(true).build();
-            cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
-                @Override
-                public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
-                    try {
-                        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA},RequestCameraPermissionId);
-                            return;
-                        }
-                        cameraSource.start(cameraView.getHolder());
-                    }catch(IOException e){
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-                }
-
-                @Override
-                public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
-                    cameraSource.stop();
-                }
-            });
-            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
-                @Override
-                public void release() {
-
-                }
-
-                @Override
-                public void receiveDetections(Detector.Detections<TextBlock> detections) {
-                    final SparseArray<TextBlock> items = detections.getDetectedItems();
-                    if(items.size() != 0){
-                        textView.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                StringBuilder stringBuilder = new StringBuilder();
-                                for(int i = 0; i< items.size(); i++){
-                                    TextBlock item = items.valueAt(i);
-                                    stringBuilder.append(item.getValue());
-                                    stringBuilder.append("/n");
-                                }
-                                textView.setText(stringBuilder.toString());
-                            }
-                        });
-                    }
-                }
-            });
-
-            }
+    @Override
+    public void onCameraViewStarted(int width, int height) {
 
     }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
 }
